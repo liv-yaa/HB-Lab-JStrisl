3ea65705d76d8ecea35c8532c52bf533240745ca
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-31 12:34:30 -0800

    First commit

---
+++ trials.js
@@ -0,0 +1,91 @@
+// ///////////////////////////////////////////////////////
+// PART 1
+// Account information:
+
+let accountHolder = "Balloonicorn";
+const accountNumber = "123456";
+let businessName = "Balloonicorn's Cupcake Shop";
+
+const addresses = ["123 Main Street", "683 Sutter Street", "1600 Pennsylvania Ave"];
+
+const phoneNumbers = new Map();
+
+// Add some phone numbers to our map
+
+
+
+// ///////////////////////////////////////////////////////
+// Create User Info Display:
+
+// Add function to print account information 
+
+
+// Add function to print all addresses, including a header
+
+
+// Add function to print phone types and numbers
+
+
+// ///////////////////////////////////////////////////////
+// Transactions:
+
+// Create an empty map of transactions
+
+
+// Add function to add transactions
+
+
+// Use the function to add transactions
+
+
+// Add function to show balance status
+
+
+// Add function to show transactions
+
+
+// ///////////////////////////////////////////////////////
+// All Customer Info:
+
+// Make an object with customer info
+
+
+// Function to add customer attributes
+
+
+// Add attributes for this user
+
+
+// ///////////////////////////////////////////////////////
+// Getting a Business Loan
+
+
+// Function to return loan rate
+
+
+// ///////////////////////////////////////////////////////
+// Account Report
+
+
+// Add function to show bank customer report
+
+
+
+// ///////////////////////////////////////////////////////
+// PART 2:
+// Bank Manager
+
+// Create map of customer addresses
+
+
+// Write a function to return the address of a given person
+
+
+// Add a function to create an employee schedule for the week 
+
+
+// Add a function for weekly hours
+
+
+
+
